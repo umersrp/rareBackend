e8b1cc4d1036fee63f3fc10325d05c22d4548bf0
muhammad676 <arsalanmuhammad676@gmail.com>
2024-01-11 18:34:55 +0500

    latest staging deployment and build route fix

--- build/assets/inheritsLoose.f2b3ab7d.js
+++
@@ -1,1 +0,0 @@
-import{au as e}from"./index.50116af3.js";function p(t,o){t.prototype=Object.create(o.prototype),t.prototype.constructor=t,e(t,o)}export{p as _};
--- build/assets/isEqual.98c88f4e.js
+++
@@ -1,1 +0,0 @@
-import{b1 as r}from"./index.50116af3.js";var u=r;function i(a,s){return u(a,s)}var l=i;export{l as i};
