37ea887d7ec58d8ebd2904f04bee7c200547626e
Ukasha18 <ukashaaziz0@gmail.com>
2024-02-19 02:56:25 +0500

    pushing build

---
+++ build/assets/DeleteParagraph.1e660ff1.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index.f47d5396.js";const r=({moduleName:a,linkedName:e})=>t("p",{children:`You don't have the access to delete this ${e}`});export{r as D};
---
+++ build/assets/placeholder.39032bbb.js
@@ -0,0 +1,1 @@
+import{c as t,j as e,C as a}from"./index.f47d5396.js";import{G as r,S as l}from"./Table.f9b3fff9.js";import{L as i}from"./ListLoading.b6450c73.js";const n=()=>t("div",{className:"space-y-5",children:[e(a,{title:"Example One",children:e(r,{count:3})}),e(a,{title:"Example Two",children:e(l,{count:3})}),e(a,{title:"Example Three",children:e(i,{count:3})})]});export{n as default};
