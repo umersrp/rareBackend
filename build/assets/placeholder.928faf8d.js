fa4e05183c451f8cf0e77581f34f5cc4ca824927
Ukasha18 <ukashaaziz0@gmail.com>
2024-02-21 18:16:07 +0500

    build

--- build/assets/placeholder.928faf8d.js
+++
@@ -1,1 +0,0 @@
-import{c as t,j as e,C as a}from"./index.2690966c.js";import{G as r,S as l}from"./Table.2669a0bc.js";import{L as i}from"./ListLoading.27e2dd30.js";const n=()=>t("div",{className:"space-y-5",children:[e(a,{title:"Example One",children:e(r,{count:3})}),e(a,{title:"Example Two",children:e(l,{count:3})}),e(a,{title:"Example Three",children:e(i,{count:3})})]});export{n as default};
--- build/assets/placeholder.e8af60e1.js
+++
@@ -1,1 +0,0 @@
-import{c as t,j as e,C as a}from"./index.cb22c4c1.js";import{G as r,S as l}from"./Table.9bc207d5.js";import{L as i}from"./ListLoading.eff91132.js";const n=()=>t("div",{className:"space-y-5",children:[e(a,{title:"Example One",children:e(r,{count:3})}),e(a,{title:"Example Two",children:e(l,{count:3})}),e(a,{title:"Example Three",children:e(i,{count:3})})]});export{n as default};
