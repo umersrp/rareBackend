00404c5ad949c336db299bbb83a97bb4c10615f0
Ukasha18 <ukashaaziz0@gmail.com>
2024-02-21 17:59:48 +0500

    changes

---
+++ build/assets/placeholder.ae2b6fe7.js
@@ -0,0 +1,1 @@
+import{c as t,j as e,C as a}from"./index.5d56e3e7.js";import{G as r,S as l}from"./Table.73fbad27.js";import{L as i}from"./ListLoading.8974b066.js";const n=()=>t("div",{className:"space-y-5",children:[e(a,{title:"Example One",children:e(r,{count:3})}),e(a,{title:"Example Two",children:e(l,{count:3})}),e(a,{title:"Example Three",children:e(i,{count:3})})]});export{n as default};
