a7831f9fc19241d54c2c2e44daa3fae33326abb6
Muhammad Ahsan <ahsan@srptechs.com>
2024-02-22 08:24:34 -0800

    staging build

--- build/assets/placeholder.d6a6c4fa.js
+++
@@ -1,1 +0,0 @@
-import{c as t,j as e,C as a}from"./index.6dc3f541.js";import{G as r,S as l}from"./Table.49a502b0.js";import{L as i}from"./ListLoading.609f64c8.js";const n=()=>t("div",{className:"space-y-5",children:[e(a,{title:"Example One",children:e(r,{count:3})}),e(a,{title:"Example Two",children:e(l,{count:3})}),e(a,{title:"Example Three",children:e(i,{count:3})})]});export{n as default};
--- build/assets/placeholder.dfc5eba0.js
+++
@@ -1,1 +0,0 @@
-import{c as t,j as e,C as a}from"./index.821ab456.js";import{G as r,S as l}from"./Table.347c9b98.js";import{L as i}from"./ListLoading.b575a222.js";const n=()=>t("div",{className:"space-y-5",children:[e(a,{title:"Example One",children:e(r,{count:3})}),e(a,{title:"Example Two",children:e(l,{count:3})}),e(a,{title:"Example Three",children:e(i,{count:3})})]});export{n as default};
