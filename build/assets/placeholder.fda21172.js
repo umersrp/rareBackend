615144bbb8c07ecaed902b04dd85207805db21ad
Ukasha18 <ukashaaziz0@gmail.com>
2024-02-23 07:28:48 +0500

    chnages

---
+++ build/assets/placeholder.fda21172.js
@@ -0,0 +1,1 @@
+import{c as t,j as e,C as a}from"./index.c54703c8.js";import{G as r,S as l}from"./Table.8262cf87.js";import{L as i}from"./ListLoading.e7392f48.js";const n=()=>t("div",{className:"space-y-5",children:[e(a,{title:"Example One",children:e(r,{count:3})}),e(a,{title:"Example Two",children:e(l,{count:3})}),e(a,{title:"Example Three",children:e(i,{count:3})})]});export{n as default};
