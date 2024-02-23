bbfb406426c1d6f04d5e6b8b11955ac9e04dbf48
Ukasha18 <ukashaaziz0@gmail.com>
2024-02-23 20:23:19 +0500

    chnages in build

---
+++ build/assets/DeleteParagraph.38214956.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index.26a0f8e4.js";var a="C:\\Users\\LENOVO\\Desktop\\rareProjects\\Rare_Frontend\\src\\pages\\my-components\\DeleteParagraph.jsx";const t=({moduleName:s,linkedName:e})=>r("p",{children:`You don't have the access to delete this ${e}`},void 0,!1,{fileName:a,lineNumber:5,columnNumber:9},void 0);export{t as D};
