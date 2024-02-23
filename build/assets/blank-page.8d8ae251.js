83fb5fbeaf33a71a5fe97081f74718b533f9978a
Ukasha18 <ukashaaziz0@gmail.com>
2024-02-23 21:44:35 +0500

    pushing build

---
+++ build/assets/blank-page.8d8ae251.js
@@ -0,0 +1,1 @@
+import{j as l}from"./index.33fc8c4f.js";const e=()=>l("div",{children:l("h4",{className:"font-medium lg:text-2xl text-xl capitalize text-slate-900 inline-block ltr:pr-4 rtl:pl-4 mb-6",children:"Blank Page"})});export{e as default};
