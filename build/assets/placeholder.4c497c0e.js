393dedc930acbee5d6ac66f7f1fc98c6c4be3dd4
Ukasha18 <ukashaaziz0@gmail.com>
2024-02-23 20:12:50 +0500

    chnages now on build

---
+++ build/assets/placeholder.4c497c0e.js
@@ -0,0 +1,1 @@
+import{c as t,j as e,C as a}from"./index.38c1a975.js";import{G as r,S as l}from"./Table.bab6848e.js";import{L as i}from"./ListLoading.1f96d3a9.js";const n=()=>t("div",{className:"space-y-5",children:[e(a,{title:"Example One",children:e(r,{count:3})}),e(a,{title:"Example Two",children:e(l,{count:3})}),e(a,{title:"Example Three",children:e(i,{count:3})})]});export{n as default};
