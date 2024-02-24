d8117e5831e4a27637664ce32e2603a4a858a069
Ukasha18 <ukashaaziz0@gmail.com>
2024-02-25 04:09:31 +0500

    pushing build

---
+++ build/assets/ThemeProvider.7be63f07.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index.189aac50.js";const r=["xxl","xl","lg","md","sm","xs"],n="xs",x=e.exports.createContext({prefixes:{},breakpoints:r,minBreakpoint:n});function c(o,t){const{prefixes:s}=e.exports.useContext(x);return o||s[t]||t}export{c as u};
---
+++ build/assets/placeholder.c16542d9.js
@@ -0,0 +1,1 @@
+import{c as t,j as e,f as a}from"./index.189aac50.js";import{G as r,S as l}from"./Table.bc7409d3.js";import{L as i}from"./ListLoading.d120741f.js";const n=()=>t("div",{className:"space-y-5",children:[e(a,{title:"Example One",children:e(r,{count:3})}),e(a,{title:"Example Two",children:e(l,{count:3})}),e(a,{title:"Example Three",children:e(i,{count:3})})]});export{n as default};
