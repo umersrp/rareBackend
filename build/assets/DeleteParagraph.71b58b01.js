4c9ac7dbb2f550a9497352c28a04cfb7e0ed2c54
Ukasha18 <ukashaaziz0@gmail.com>
2024-02-24 07:16:27 +0500

    changes in builds

---
+++ build/assets/DeleteParagraph.71b58b01.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index.3e2a71f6.js";const r=({moduleName:a,linkedName:e})=>t("p",{children:`You don't have the access to delete this ${e}`});export{r as D};
---
+++ build/assets/video.b9958a2c.js
@@ -0,0 +1,1 @@
+import{r,j as e}from"./index.3e2a71f6.js";function n({url:s,className:a="w-full"}){const[t,l]=r.exports.useState(!1);return e("div",{className:"w-full relative",children:e("video",{src:s,onClick:()=>{l(!t)},controls:!0,className:a})})}const c=()=>e("div",{className:"div",children:e(n,{url:"https://vjs.zencdn.net/v/oceans.mp4"})});export{c as default};
