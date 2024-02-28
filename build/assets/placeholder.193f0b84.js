603f2fb3966f99bcfc4c347dc077d4813684fc7d
Ukasha18 <ukashaaziz0@gmail.com>
2024-02-28 12:12:55 +0500

    artat work

---
+++ build/assets/placeholder.193f0b84.js
@@ -0,0 +1,1 @@
+import{c as t,j as e,f as a}from"./index.11981b5d.js";import{G as r,S as l}from"./Table.c30ef2b5.js";import{L as i}from"./ListLoading.dc2d1bd8.js";const n=()=>t("div",{className:"space-y-5",children:[e(a,{title:"Example One",children:e(r,{count:3})}),e(a,{title:"Example Two",children:e(l,{count:3})}),e(a,{title:"Example Three",children:e(i,{count:3})})]});export{n as default};
