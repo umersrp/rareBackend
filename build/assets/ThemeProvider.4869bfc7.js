20847eff328fcec792991798e3273170e74f29e5
Ukasha18 <ukashaaziz0@gmail.com>
2024-02-29 12:28:49 +0500

    chnages

---
+++ build/assets/ThemeProvider.4869bfc7.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index.a80d7db0.js";const r=["xxl","xl","lg","md","sm","xs"],n="xs",x=e.exports.createContext({prefixes:{},breakpoints:r,minBreakpoint:n});function c(o,t){const{prefixes:s}=e.exports.useContext(x);return o||s[t]||t}export{c as u};
