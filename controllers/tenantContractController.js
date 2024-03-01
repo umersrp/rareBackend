63fb9c08c58896b3db602950713d40d3efc37fa8
Ukasha18 <ukashaaziz0@gmail.com>
2024-03-01 22:37:15 +0500

    chnages

--- controllers/tenantContractController.js
+++ controllers/tenantContractController.js
@@ -850,8 +850,7 @@ const updateTenantContractCancel = asyncHandler(async (req, res) => {
       await TenantContract.updateOne(
         {_id : id },
         {$set: { 
-          contractupdation : "terminated",
-          softdelete : true
+          contractupdation : "terminated"
          }},
         {new : true})
   
