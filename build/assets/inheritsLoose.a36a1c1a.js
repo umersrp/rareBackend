6187b8417896e9769684e5f7edb4f6da0592fabd
Ukasha18 <ukashaaziz0@gmail.com>
2024-03-05 15:59:12 +0500

    changes now

---
+++ build/assets/inheritsLoose.a36a1c1a.js
@@ -0,0 +1,1 @@
+import{aB as e}from"./index.4113582e.js";function p(t,o){t.prototype=Object.create(o.prototype),t.prototype.constructor=t,e(t,o)}export{p as _};
---
+++ build/assets/placeholder.f89a37ec.js
@@ -0,0 +1,1 @@
+import{c as t,j as e,f as a}from"./index.4113582e.js";import{G as r,S as l}from"./Table.036c8e97.js";import{L as i}from"./ListLoading.762a5b7f.js";const n=()=>t("div",{className:"space-y-5",children:[e(a,{title:"Example One",children:e(r,{count:3})}),e(a,{title:"Example Two",children:e(l,{count:3})}),e(a,{title:"Example Three",children:e(i,{count:3})})]});export{n as default};
