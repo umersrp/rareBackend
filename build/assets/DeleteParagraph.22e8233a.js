cb273e169e9cfc1f2f84055ba40cb6f8516adb5a
Ukasha18 <ukashaaziz0@gmail.com>
2024-03-05 16:05:52 +0500

    chanes now on

---
+++ build/assets/DeleteParagraph.22e8233a.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index.e63d7583.js";var a="C:\\Users\\LENOVO\\Desktop\\rareProjects\\Rare_Frontend\\src\\pages\\my-components\\DeleteParagraph.jsx";const t=({moduleName:s,linkedName:e})=>r("p",{children:`You don't have the access to delete this ${e}`},void 0,!1,{fileName:a,lineNumber:5,columnNumber:9},void 0);export{t as D};
