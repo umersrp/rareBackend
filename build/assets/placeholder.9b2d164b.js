d572bfe85b8d73d3377c9068450999a5c700d3d5
Ukasha18 <ukashaaziz0@gmail.com>
2024-03-05 17:58:38 +0500

    'chanes'

---
+++ build/assets/placeholder.9b2d164b.js
@@ -0,0 +1,1 @@
+import{c as t,j as e,f as a}from"./index.59da7a86.js";import{G as r,S as l}from"./Table.1097b97d.js";import{L as i}from"./ListLoading.8cac3c57.js";const n=()=>t("div",{className:"space-y-5",children:[e(a,{title:"Example One",children:e(r,{count:3})}),e(a,{title:"Example Two",children:e(l,{count:3})}),e(a,{title:"Example Three",children:e(i,{count:3})})]});export{n as default};
