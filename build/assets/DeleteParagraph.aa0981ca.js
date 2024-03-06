4b9e0405062c22fcdec002d61eadc2bc4940e788
Ukasha18 <ukashaaziz0@gmail.com>
2024-03-06 16:23:26 +0500

    changes in bild pro

---
+++ build/assets/DeleteParagraph.aa0981ca.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index.a4717e99.js";const r=({moduleName:a,linkedName:e})=>t("p",{children:`You don't have the access to delete this ${e}`});export{r as D};
---
+++ build/assets/inheritsLoose.6b3176cb.js
@@ -0,0 +1,1 @@
+import{aB as e}from"./index.a4717e99.js";function p(t,o){t.prototype=Object.create(o.prototype),t.prototype.constructor=t,e(t,o)}export{p as _};
