030bd243166864853a1b95b30d990acb5323eef1
Ukasha18 <ukashaaziz0@gmail.com>
2024-03-11 02:11:41 +0500

    chnages from stagging

---
+++ build/assets/placeholder.86f87170.js
@@ -0,0 +1,1 @@
+import{c as t,j as e,f as a}from"./index.889dfc92.js";import{G as r,S as l}from"./Table.23ee01da.js";import{L as i}from"./ListLoading.47f6b4f2.js";const n=()=>t("div",{className:"space-y-5",children:[e(a,{title:"Example One",children:e(r,{count:3})}),e(a,{title:"Example Two",children:e(l,{count:3})}),e(a,{title:"Example Three",children:e(i,{count:3})})]});export{n as default};
