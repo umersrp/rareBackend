3835d7c4e479691645f84869de7604c1f776785c
Ukasha18 <ukashaaziz0@gmail.com>
2024-03-18 16:29:20 +0500

    chnages in build

---
+++ build/assets/inheritsLoose.b9546d6e.js
@@ -0,0 +1,1 @@
+import{aB as e}from"./index.a065b7d1.js";function p(t,o){t.prototype=Object.create(o.prototype),t.prototype.constructor=t,e(t,o)}export{p as _};
---
+++ build/assets/placeholder.f4978315.js
@@ -0,0 +1,1 @@
+import{c as t,j as e,f as a}from"./index.a065b7d1.js";import{G as r,S as l}from"./Table.4e518232.js";import{L as i}from"./ListLoading.74b2ab94.js";const n=()=>t("div",{className:"space-y-5",children:[e(a,{title:"Example One",children:e(r,{count:3})}),e(a,{title:"Example Two",children:e(l,{count:3})}),e(a,{title:"Example Three",children:e(i,{count:3})})]});export{n as default};
